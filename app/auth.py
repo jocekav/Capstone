837f68097b3de7e45d5b4740ff407c19464683c4
jocekav <jkavanagh3@gatech.edu>
2021-10-31 20:57:44 -0400

    Missed files from previous commit and gitignore update

---
+++ app/auth.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from __init__ import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':    
+        return render_template('login.html')
+    else:
+        # if the request is POST the we check if the user exist 
+          # and with te right password
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+        user = User.query.filter_by(email=email).first()
+        # check if the user actually exists
+        # take the user-supplied password, hash it, and compare it     
+        # to the hashed password in the database
+        if not user:
+            # flash('Please sign up before!')
+            print('Please sign up before!')
+            return redirect(url_for('auth.signup'))
+        elif not check_password_hash(user.password, password):
+            # flash('Please check your login details and try again.')
+            print('Please check your login details and try again.')
+            return redirect(url_for('auth.login')) # if the user 
+               #doesn't exist or password is wrong, reload the page
+        # if the above check passes, then we know the user has the 
+        # right credentials
+        login_user(user, remember=remember)
+        return redirect(url_for('main.profile'))
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            # flash('Email already exists')
+            print('email already exists')
+            return redirect(url_for('auth.signup'))
+        new_user = User(email=email, name=name,
+                        password=generate_password_hash(password,
+                        method='sha256'), location='blank', preference='blank', age='blank')
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
---
+++ app/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models import User
+
+import os
+import secrets
+from PIL import Image
+
+class UpdateProfileForm(FlaskForm):
+    location = StringField('Location',
+                           validators=[DataRequired()])
+    preference = StringField('Preference',
+                        validators=[DataRequired()])
+    age = StringField('age',
+                        validators=[DataRequired()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    # def validate_username(self, username):
+    #     if username.data != current_user.username:
+    #         user = User.query.filter_by(username=username.data).first()
+    #         if user:
+    #             raise ValidationError('That username is taken. Please choose a different one.')
+
+    # def validate_email(self, email):
+    #     if email.data != current_user.email:
+    #         user = User.query.filter_by(email=email.data).first()
+    #         if user:
+    #             raise ValidationError('That email is taken. Please choose a different one.')
+
+    def save_picture(form_picture, type):
+        random_hex = secrets.token_hex(8)
+        _, f_ext = os.path.splitext(form_picture)
+        picture_fn = random_hex + f_ext
+        picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+        output_size = (125, 125)
+        i = Image.open(form_picture)
+        i.thumbnail(output_size)
+        i.save(picture_path)
+
+        return picture_fn
