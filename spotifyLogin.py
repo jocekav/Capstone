bbeb5cfa76ee5a254de957b4fe681d989565318d
RoseQSun <70708796+RoseQSun@users.noreply.github.com>
2021-10-31 20:53:01 -0400

    Add files via upload

---
+++ spotifyLogin.py
@@ -0,0 +1,59 @@
+import requests
+import json
+
+##server side
+CLIENT_ID = "64d88f30bd6e47d5a99e11985fdd0bdb"
+CLIENT_SECRET = "aa57afecfba4466bb0caeae430352b4c"
+PORT = "5000"
+CLIENT_URL = "http://127.0.0.1"
+REDIRECT_URI = "{}:{}/player/".format(CLIENT_URL, PORT)
+SCOPE = "user-modify-playback-state user-read-recently-played streaming user-read-currently-playing user-read-playback-state user-read-email user-read-private"
+TOKEN_DATA = []
+
+##spotify parameters
+SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
+SPOTIFY_URL_TOKEN = 'https://accounts.spotify.com/api/token'
+RESPONSE_TYPE = 'code'   
+HEADER = 'application/x-www-form-urlencoded'
+REFRESH_TOKEN = ''
+
+
+def getAuth():
+    return "{}client_id={}&response_type={}&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, CLIENT_ID, RESPONSE_TYPE, REDIRECT_URI, SCOPE) 
+
+def getUserToken(authCode):
+    global TOKEN_DATA
+    body = {
+        "grant_type": "authorization_code",
+        "code": str(authCode),
+        "redirect_uri": REDIRECT_URI,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    }
+
+    postRequest = requests.post(SPOTIFY_URL_TOKEN, data=body)
+    response = json.loads(postRequest.text)
+    TOKEN_DATA = handleToken(response)
+    return TOKEN_DATA
+
+def refreshToken(time):
+    time.sleep(time)
+    body = {
+        "grant_type": "refresh_token",
+        "refresh_token": REFRESH_TOKEN
+    }
+    postRefresh = requests.post(SPOTIFY_URL_TOKEN, data=body)
+    response = json.dumps(postRefresh.text)
+    TOKEN_DATA = handleToken(response)
+    return TOKEN_DATA
+
+def handleToken(response):
+    global auth_head
+    auth_head = {'Accept': 'application/json',
+                'Content-Type': 'application/json', 
+                "Authorization": "Bearer {}".format(response["access_token"])}
+    REFRESH_TOKEN = response["refresh_token"]
+    return [response["access_token"], auth_head, response["scope"], response["expires_in"]]
+
+def getAccessToken():
+    return TOKEN_DATA
