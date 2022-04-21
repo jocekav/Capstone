8e6bbdd4feb9a3f9ce9d8bdeaf7a526ef6093c9d
jocekav <jkavanagh3@gatech.edu>
2022-04-21 08:57:41 -0400

    Switch to SQLalchemy and try heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,41 +1,48 @@
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 from flask_login import LoginManager
 
 # init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
+# def create_app():
+app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config.from_object(Config)
 
-    db.init_app(app)
+DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
+db = SQLAlchemy(app)
+# engine = create_engine(DB_URI)
+# app.config['SECRET_KEY'] = 'secret-key-goes-here'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    login_manager = LoginManager() # Create a Login Manager instance
-    login_manager.login_view = 'auth.login' # define the redirection 
-                         # path when login required and we attempt 
-                         # to access without being logged in
-    login_manager.init_app(app) # configure it for login
-    
-    from models import User
-    @login_manager.user_loader
-    def load_user(user_id): #reload user object from the user ID 
-                            #stored in the session
-        # since the user_id is just the primary key of our user 
-        # table, use it in the query for the user
-        return User.query.get(int(user_id))
-    
-    # blueprint for auth routes in our app
-    from auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+# db.init_app(app)
 
-    # blueprint for non-auth parts of app
-    from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+login_manager = LoginManager() # Create a Login Manager instance
+login_manager.login_view = 'auth.login' # define the redirection 
+                        # path when login required and we attempt 
+                        # to access without being logged in
+login_manager.init_app(app) # configure it for login
 
-    from match import match as match_blueprint
-    app.register_blueprint(match_blueprint)
+from models import User
+@login_manager.user_loader
+def load_user(user_id): #reload user object from the user ID 
+                        #stored in the session
+    # since the user_id is just the primary key of our user 
+    # table, use it in the query for the user
+    return User.query.get(int(user_id))
 
-    return app
+# blueprint for auth routes in our app
+from auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+from match import match as match_blueprint
+app.register_blueprint(match_blueprint)
+
+    # return app
