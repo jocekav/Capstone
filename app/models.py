1446227e9aefe01f86fb3948bb94923805057e2f
jocekav <jkavanagh3@gatech.edu>
2022-04-21 10:14:02 -0400

    Update user class for database

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from __init__ import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(1000))
     location = db.Column(db.String(1000))
     preference = db.Column(db.String(1000))
