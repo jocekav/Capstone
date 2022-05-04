e72794c13a4d43d0a010317eeffdd6df7d340b29
jocekav <jkavanagh3@gatech.edu>
2022-05-03 23:17:10 -0400

    Changing file structure

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import db, app
+# from app import db, app
 # from main import app
+import app
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, app.db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
