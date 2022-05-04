b1c925ef39f1ea73611304cd2b62f662fc3f8d68
jocekav <jkavanagh3@gatech.edu>
2022-05-03 23:22:04 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate, MigrateCommand
 from app.main import app
 from app import db
 
-migrate = Migrate(app, app.db)
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
